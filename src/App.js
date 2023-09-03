bfa9f47f8fb1248316afa82b7afde06973484186
TarifSadman <sadmantaha17@gmail.com>
2023-09-03 17:26:40 +0600

    testing

--- src/App.js
+++ src/App.js
@@ -16,11 +16,21 @@ function Counter(props) {
     const isAppleDevice = () => /Mac|iPod|iPhone|iPad/.test(navigator.userAgent)
     console.log(isAppleDevice());
 
+    const checkTabInView = () => !document.hidden
+    console.log(checkTabInView());
+
+    const generateRandomIP = () => {
+        return Array.from({length: 4}, () => Math.floor(Math.random() * 256)).join('.');
+      }
+      
+      generateRandomIP() // 220.187.184.113
+      generateRandomIP() // 254.24.179.151
+
     // isAppleDevice() // true or false
 
     return(
         <div style={{textAlign: 'center'}}>
-            <h3>Counter App</h3>
+            <h3>A Random IP: {generateRandomIP()}</h3>
             <div style={{ marginBottom: 10 }}>
                 <Button variant="contained" onClick={increment}>+</Button>
                 <span> {count} </span>
@@ -28,6 +38,9 @@ function Counter(props) {
             </div>
             <div style={{ marginBottom: 10 }}>
             {isAppleDevice() ? 'Apple Device' : 'Not Apple Device'}
+            </div>
+            <div style={{ marginBottom: 10 }}>
+            {checkTabInView() ? 'Tab is in view' : 'Tab is not in view'}
             </div>
             <TextField fullWidth label="fullWidth" id="fullWidth" />
         </div>
